9312df28a8856558bbde59c9b645fbf43e1a0c4e
Emil Ribarev <eiribarev18@codingburgas.bg>
2021-07-10 12:41:36 +0300

    Begin implementing the Project class

---
+++ coms-tup-application/coms-tup-application/Project.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <unordered_set>
+
+class Project {
+  public:
+	Project(int32_t createdBy);
+	Project(const std::string &name, const std::string &description, int32_t createdBy);
+
+	void setName(const std::string &name, int32_t changedBy);
+	void setDescription(const std::string &description, int32_t changedBy);
+
+	std::string getName() const;
+	std::string getDescription() const;
+
+  private:
+	std::string name;
+	std::string description;
+	const std::string createdOn;
+	const int32_t createdBy;
+	std::string lastChangedOn;
+	std::string lastChangedBy;
+
+	std::unordered_set<int32_t> taskIDs;
+
+	void setName(const std::string &name);
+	void setDescription(const std::string &description);
+
+	template <typename T>
+	friend void touch(T &, int32_t);
+};
