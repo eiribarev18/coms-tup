cc2afccbf2fefd47036db78b4e50b15c5a99fa33
Emil Ribarev <eiribarev18@codingburgas.bg>
2021-07-13 21:33:27 +0300

    Add function for reading unsigned numbers

--- coms-tup-application/coms-tup-application/console_io.cpp
+++ coms-tup-application/coms-tup-application/console_io.cpp
@@ -5,6 +5,28 @@
 
 using namespace std;
 
+void getUnsignedNumber(size_t &x, size_t maxValue = -1, size_t minValue = 0, char delimiter = '\n')
+{
+	std::string raw;
+
+	getline(cin, raw, delimiter);
+
+	for (size_t i = 0; i < raw.size(); i++) {
+		if (raw[i] < '0' or raw[i] > '9') throw runtime_error("Non-digit character encountered");
+	}
+
+	if (stoull(raw) > maxValue) {
+		throw out_of_range("Number " + to_string((unsigned long long)x) +
+						   " is larger than maxValue=" + to_string(maxValue));
+	}
+	if (stoull(raw) < minValue) {
+		throw out_of_range("Number " + to_string((unsigned long long)x) +
+						   " is smaller than minValue=" + to_string(maxValue));
+	}
+
+	x = stoull(raw);
+}
+
 size_t showMenuOptions(const std::vector<MENU_OPTION> &options, const User &loggedUser)
 {
 	size_t num = 1;
