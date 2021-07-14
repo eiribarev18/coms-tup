665caeec999a4af9df202c0f9c28855c66dd4cdf
Emil Ribarev <eiribarev18@codingburgas.bg>
2021-07-14 14:01:58 +0300

    Make getUnsignedNumber() a template function
    So it can deal with arguments of different unsigned types

--- coms-tup-application/coms-tup-application/console_io.h
+++ coms-tup-application/coms-tup-application/console_io.h
@@ -25,3 +25,28 @@ void createUserMenu(DBManager &db, User &loggedUser);
 void clearConsole();
 void toggleEcho();
 void printNewlines(unsigned count = 1);
+
+template <typename T>
+void getUnsignedNumber(T &x, size_t maxValue = -1, size_t minValue = 0, char delimiter = '\n')
+{
+	using namespace std;
+
+	std::string raw;
+
+	getline(cin, raw, delimiter);
+
+	for (size_t i = 0; i < raw.size(); i++) {
+		if (raw[i] < '0' or raw[i] > '9') throw runtime_error("Non-digit character encountered");
+	}
+
+	if (stoull(raw) > maxValue) {
+		throw out_of_range("Number " + to_string((unsigned long long)x) +
+						   " is larger than maxValue=" + to_string(maxValue));
+	}
+	if (stoull(raw) < minValue) {
+		throw out_of_range("Number " + to_string((unsigned long long)x) +
+						   " is smaller than minValue=" + to_string(maxValue));
+	}
+
+	x = (T)stoull(raw);
+}
